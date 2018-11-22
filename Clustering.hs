96d28ff80e079eea2ee3d36a62758ac3a0edb7bd
Matthew <mdeardorff123@gmail.com>
2018-11-21 23:15:45 -0600

    KNN working and graded

--- Clustering.hs
+++ Clustering.hs
@@ -5,12 +5,6 @@ import Data.Function
 import Data.List (group, sort, nub, groupBy,sortBy)
 import System.Random
 
-dataSet :: [[Double]]
-dataSet = [[2,2],[2,1],[0,0],[1,0],[0,1],[1,2]]
-
-
---run = kmeans 2 dataSet
-
 data Vals = Numeric Double | Category Int deriving (Eq, Ord)
 type Features = [Vals]
 
@@ -29,20 +23,45 @@ mean vs = Numeric ((add 0 vs) / fromIntegral (length vs))
     add acc [] = acc
     add acc ((Numeric x):xs) = add (acc + x) xs
 
-diff x y
-  | x == y = 0.0
-  | otherwise = 1.0
-
 kmeans k r = do
   d <- parseData
   g <- newStdGen
   let ranges = calcRanges d
       normalizeD = map (normalize ranges) d
       rs = take k . nub $ (randomRs (1,length d) g :: [Int]) --random indices for initial centroids
-      cs = getCentroids rs normalizeD
-  finalCentroids <- loop cs normalizeD r
-  return (finalCentroids)
+      cs = getCentroids rs normalizeD --initialize centroids
+  results <- loop cs normalizeD r --begin iterating loop
+  return (results)
+  where
+    loop cs d r = do
+      let assignments = groupBy ((==) `on` snd) (sortBy (second) (map (assign cs r) d))
+          newCentroids = map (calcPrototype . (map fst)) assignments
+      if(newCentroids == cs)
+        then do
+          results <- evaluate newCentroids r
+          performance results
+        else do
+          putStrLn "Calculating..."
+          loop newCentroids d r
 
+evaluate centroids r = do
+  d <- parseDataFull
+  let ranges = calcRanges (map fst d)
+      normalizeD = map (\(x,i) -> (normalize ranges x, i)) (d)
+      evals = map (eval centroids r) (normalizeD)
+  return evals
+
+performance results = do
+  let correct = length $ filter ((==) True) results
+      possible = length results
+      percentage = fromIntegral correct / fromIntegral possible
+  putStrLn ((show correct) ++ " out of " ++ (show possible))
+  putStrLn ((show percentage) ++ "% accuracy")
+
+eval :: [(Features)] -> Double -> (Features, Int) -> Bool
+eval cents r (f,c) = if ((snd $ assign cents r f) == c)
+  then True
+  else False
 
 
   --assignments = groupBy ((==) `on` snd) (sortBy (second) (map (assign cs) d))
@@ -51,14 +70,6 @@ kmeans k r = do
 
 second a b = compare (snd a) (snd b)
 
-loop cs d r = do
-  let assignments = groupBy ((==) `on` snd) (sortBy (second) (map (assign cs r) d))
-      newCentroids = map (calcPrototype . (map fst)) assignments
-  if(newCentroids == cs)
-    then return newCentroids
-    else do
-      putStrLn "Looping..."
-      loop newCentroids d r
 
 --printLength :: [[(Features, Int)]]
 printLength (a:as) = do
@@ -70,9 +81,10 @@ assign :: [Features] -> Double -> Features -> (Features,Int)
 assign cs r v = helper [] v cs r
   where
     helper acc v [] r = (v,minIndex acc)
-    helper acc v (c:cs) r = helper (acc ++ [(calcDistance v c r)]) v cs r
+    helper acc v (c:cs) r = helper (acc ++ [(calcDistance r v c)]) v cs r
 
 minIndex xs = head $ filter ((== minimum xs) . (xs !!)) [0..]
+maxIndex xs = head $ filter ((== maximum xs) . (xs !!)) [0..]
 
 --given the entire initial dataset, find min and max values for numeric features
 calcRanges :: [Features] -> [(Double,Double)]
@@ -102,7 +114,7 @@ getCentroids rs d = helper [] rs d
     helper acc (r:rs) d = helper (acc ++ [(d `at` r)]) rs d
 
 --Indexing for nested lists
-at :: [Features] -> Int -> Features
+at :: [a] -> Int -> a
 at v i = helper 0 v i
   where
     helper c (v:vs) i
@@ -111,8 +123,8 @@ at v i = helper 0 v i
       | otherwise = error "Index too large"
 
 --calculate distance between feature vectors given a constant for categorical significance
-calcDistance :: Features -> Features -> Double -> Double
-calcDistance v1 v2 r = helper 0.0 v1 v2 r
+calcDistance :: Double -> Features -> Features -> Double
+calcDistance r v1 v2 = helper 0.0 v1 v2 r
   where
     helper dist [] [] _ = dist
     helper dist ((Category v):vs) ((Category w):ws) r = helper (((diff v w) * r) + dist) vs ws r
@@ -136,6 +148,9 @@ parseLine :: [String] -> Features
 parseLine (age:wclass:fnlwgt:ed:ednum:mstat:occ:rel:race:sex:cgain:closs:hpw:nc:_) = [Numeric (read age), Category (catVal empK wclass), Numeric ((read fnlwgt)), Category (catVal eduK ed), Numeric ((read ednum)), Category (catVal mstatusK mstat), Category (catVal occK occ), Category (catVal raceK race), Category (catVal genderK sex), Numeric ((read cgain)), Numeric ((read closs)), Numeric ((read hpw)), Category (catVal countryK nc)]
 parseLine _ = []
 
+parseLineFull :: [String] -> (Features, Int)
+parseLineFull (age:wclass:fnlwgt:ed:ednum:mstat:occ:rel:race:sex:cgain:closs:hpw:nc:label:_) = ([Numeric (read age), Category (catVal empK wclass), Numeric ((read fnlwgt)), Category (catVal eduK ed), Numeric ((read ednum)), Category (catVal mstatusK mstat), Category (catVal occK occ), Category (catVal raceK race), Category (catVal genderK sex), Numeric ((read cgain)), Numeric ((read closs)), Numeric ((read hpw)), Category (catVal countryK nc)], catVal labelK label)
+
 parseData = do
   d <- readFile "adult.data"
   let l = lines d
@@ -143,9 +158,20 @@ parseData = do
       feat = map (parseLine) w
   return feat
 
+parseDataFull = do
+  d <- readFile "validation.test"
+  let l = lines d
+      w = map (splitBy ',') l
+      feat = map (parseLineFull) w
+  return feat
+
 splitBy delimiter = foldr f [[]]
         where f c l@(x:xs) | c == delimiter = []:l
                          | otherwise = (c:x):xs
+
+diff x y
+ | x == y = 0.0
+ | otherwise = 1.0
 
 --get value for given category and key
 catVal :: [String] -> String -> Int
@@ -164,3 +190,4 @@ relK = [" Wife", " Own-child", " Husband", " Not-in-family", " Other-relative", 
 raceK = [" White", " Asian-Pac-Islander", " Amer-Indian-Eskimo", " Other", " Black", " ?"]
 genderK = [" Female", " Male", " ?"]
 countryK = [" United-States", " Cambodia", " England", " Puerto-Rico", " Canada", " Germany", " Outlying-US(Guam-USVI-etc)", " India", " Japan", " Greece", " South", " China", " Cuba", " Iran", " Honduras", " Philippines", " Italy", " Poland", " Jamaica", " Vietnam", " Mexico", " Portugal", " Ireland", " France", " Dominican-Republic", " Laos", " Ecuador", " Taiwan", " Haiti", " Columbia", " Hungary", " Guatemala", " Nicaragua", " Scotland", " Thailand", " Yugoslavia", " El-Salvador", " Trinadad&Tobago", " Peru", " Hong", " Holand-Netherlands", " ?"]
+labelK = [" <=50K", " >50K"]
---
+++ KNN.hs
@@ -0,0 +1,70 @@
+module KNN where
+
+import Clustering
+import Data.List(sortBy, group, groupBy)
+
+main = knn 10 4.0
+
+knn k r = do
+  d <- readData
+  testD <- readTest
+  let ranges = calcRanges (map fst d)
+      testRanges = calcRanges (map fst testD)
+      normalizeD = map (\(x,i) -> (normalize ranges x, i)) d
+      normalizeTest = map (\(x,i) -> (normalize testRanges x, i)) testD
+      neighbors = map (\x -> ((getKNeighbors r k x normalizeD), snd x)) normalizeTest --map (\(x,i) -> (getKNeighbors r k x normalizeD,i)) (normalizeTest)
+      assignments = map (\x -> (vote (fst x), snd x)) neighbors
+  return assignments --grade assignments
+
+grade :: [(Int,Int)] -> IO ()
+grade list = do
+  let correct = length $ filter ((==) True) (map (\x -> (fst x == snd x)) list)
+      possible = length list
+      percentage = fromIntegral correct / fromIntegral possible
+  putStrLn ((show correct) ++ " out of " ++ (show possible))
+  putStrLn ((show percentage) ++ "% accuracy")
+  --HMMMMMMMMMMMMMMM
+
+
+--makeAssignments ::
+
+getKNeighbors :: Double -> Int -> (Features, Int) -> [(Features,Int)] -> [(Features,Int)]
+getKNeighbors r k f d = take k (sortBy (distanceFrom f r) d)-- helper [] r k f d
+  where
+    helper acc _ 0 _ _ = acc
+    helper acc r k f d = do
+      let mindex = (minIndex (map (calcDistance r (fst f)) (map fst d)))
+      helper ((d `at` mindex):acc) r (k-1) f (deleteN mindex d)
+--
+
+--helper :: [] -> Double -> Int -> (Features, Int) -> [(Features, Int)] -> [(Features,Int)]
+helper acc _ 0 _ _ = acc
+helper acc r k f d = do
+  let mindex = (minIndex (map (calcDistance r (fst f)) (map fst d)))
+  helper ((d `at` mindex):acc) r (k-1) f (deleteN mindex d)
+
+deleteN :: Int -> [a] -> [a]
+deleteN _ []     = []
+deleteN i (a:as)
+   | i == 0    = as
+   | otherwise = a : deleteN (i-1) as
+
+distanceFrom :: (Features, Int) -> Double -> (Features, Int) -> (Features, Int) -> Ordering --sort the distance of two feature vectors from a base vector and category weight
+distanceFrom f r v1 v2 = compare (calcDistance r (fst f) (fst v1)) (calcDistance r (fst f) (fst v2))
+
+--vote :: [(Features, Int)] -> Int
+vote f = maxIndex $ map length (groupBy (\a b -> snd a == snd b) (sortBy (second) f))
+
+readData = do
+  d <- readFile "adult.data"
+  let l = lines d
+      w = map (splitBy ',') l
+      feat = map (parseLineFull) w
+  return feat
+
+readTest = do
+  d <- readFile "validation.test"
+  let l = lines d
+      w = map (splitBy ',') l
+      feat = map (parseLineFull) w
+  return feat
