003380c216a1ab83f81a184b7975ac8965dd7130
Matthew <mdeardorff123@gmail.com>
2018-11-25 11:51:38 -0600

    reverse optimization...

--- KNN.hs
+++ KNN.hs
@@ -29,19 +29,19 @@ grade list = do
 --makeAssignments ::
 
 getKNeighbors :: Double -> Int -> (Features, Int) -> [(Features,Int)] -> [(Features,Int)]
-getKNeighbors r k f d = take k (sortBy (distanceFrom f r) d)-- helper [] r k f d
+getKNeighbors r k f d = helper [] r k f d (map (calcDistance r (fst f)) (map fst d)) --take k (sortBy (distanceFrom f r) d)-- helper [] r k f d
   where
-    helper acc _ 0 _ _ = acc
-    helper acc r k f d = do
-      let mindex = (minIndex (map (calcDistance r (fst f)) (map fst d)))
-      helper ((d `at` mindex):acc) r (k-1) f (deleteN mindex d)
+    helper acc _ 0 _ _ mDistance = acc
+    helper acc r k f d mDistance = do
+      let mindex = (minIndex (mDistance))
+      helper ((d `at` mindex):acc) r (k-1) f (deleteN mindex d) (deleteN mindex mDistance)
 --
 
 --helper :: [] -> Double -> Int -> (Features, Int) -> [(Features, Int)] -> [(Features,Int)]
-helper acc _ 0 _ _ = acc
-helper acc r k f d = do
-  let mindex = (minIndex (map (calcDistance r (fst f)) (map fst d)))
-  helper ((d `at` mindex):acc) r (k-1) f (deleteN mindex d)
+helper acc _ 0 _ _ _ = acc
+helper acc r k f d mDistance = undefined --do
+  --let mindex = (minIndex (map (calcDistance r (fst f)) (map fst d)))
+  --helper ((d `at` mindex):acc) r (k-1) f (deleteN mindex d)
 
 deleteN :: Int -> [a] -> [a]
 deleteN _ []     = []
@@ -50,7 +50,7 @@ deleteN i (a:as)
    | otherwise = a : deleteN (i-1) as
 
 distanceFrom :: (Features, Int) -> Double -> (Features, Int) -> (Features, Int) -> Ordering --sort the distance of two feature vectors from a base vector and category weight
-distanceFrom f r v1 v2 = compare (calcDistance r (fst f) (fst v1)) (calcDistance r (fst f) (fst v2))
+distanceFrom f r v1 v2  = compare (calcDistance r (fst f) (fst v1)) (calcDistance r (fst f) (fst v2))
 
 --vote :: [(Features, Int)] -> Int
 vote f = maxIndex $ map length (groupBy (\a b -> snd a == snd b) (sortBy (second) f))
